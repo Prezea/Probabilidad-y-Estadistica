64c8c906a5945f08ceca665d4924114446656fa6
Yendi <65693440+Prezea@users.noreply.github.com>
2020-10-21 11:31:40 -0500

    Mi primer Script
    Este será tu primer script en R. Suerte!!

---
+++ Mi primer script.R
@@ -0,0 +1,11 @@
+#1.- Vamos a crea un vector  que contenga 5 variables cuantitativas discretas,
+#contenidas en un variable llamada muestra
+
+muestra<-c(10,35,20,18,15)
+
+#2.- Vamos a calcular la media
+mean(muestra)
+
+#3.- Guardar el archivo en el proyecto
+
+#4.- Subirlo a tu repositorio de GitHub
