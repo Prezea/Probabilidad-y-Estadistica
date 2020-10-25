866ff264366fcc6cf9521c178ff03dac99921296
Yendi <65693440+Prezea@users.noreply.github.com>
2020-10-24 20:54:22 -0500

    Examinación práctica Unidad 1
    Completa el script con los comandos que sean necesarios.

---
+++ Examen Unidad 1.R
@@ -0,0 +1,49 @@
+#Examinación de la unidad 1, inserta el código para
+#activar lo que se te indica. Guarda tu script y envíalo
+#por classroom antes del martes.
+#Importante: En caso de detectar plagio, se te anulará
+#el apartado correspondiente.
+
+
+#Vas a trabajar con la matriz de datos
+#"millas" que se te proporciona a continuación.
+
+millas<-datos::millas
+
+#Realizar los siguientes pasos:
+
+
+#1.- Explora la matriz de datos millas 
+#valor: 5 puntos por comando correcto.
+
+
+#2.- Determina las medidas estadísticas descriptivas
+#y de variablidad
+#Importante: no calcules los errores.
+#Valor: 5 puntos por comando correcto.
+
+
+
+#3.- Construye:
+
+#A) Un gráfico de dispersión con las variables cilindrada y autopista
+    #el color lo indica la variable clase. 
+    #Valor Total: 20 puntos
+    #(10 puntos por comando activado correcto).
+    #(10 punto por la generación del gráfico).
+
+
+#B) Un gráfico boxplot con las variables transmisión y ciudad
+    #El color lo indica la variable transmisión    
+    #El título del gráfico es Transmisión de autos en ciudad
+    #Coloca la etiqueta "Transmisión" en el eje x 
+    #Coloca la etiqueta "Ciudad" en el eje y 
+    #Valor Total: 20 puntos
+    #(10 puntos por comando activado correcto).
+    #(10 punto por la generación del gráfico del gráfico).
+
+
+#C) Un gráfico de barras para la variable "cilindros" 
+    #Valor Total: 20 puntos
+    #(10 puntos por comando activado correcto).
+    #(10 punto por la generación del gráfico).
