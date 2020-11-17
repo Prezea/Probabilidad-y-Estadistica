2fb94ad375484d7fd429ece26f802b76f7a3b19b
Yendi <65693440+Prezea@users.noreply.github.com>
2020-11-16 22:00:07 -0600

    Muestreo aleatorio simple / estratificado

---
+++ aleatorios.R
@@ -0,0 +1,42 @@
+
+#generar numero aleatorios para una muestra de 35 personas en una 
+#población de 73 alumnas/os del 5to semestre de la LEP BENV
+
+sample(x, side, replace=True/FALSE)
+
+#Ejemplo
+sample(1:73,35, replace = FALSE)
+sample(1:70,34, replace=FALSE)
+
+
+#Ejercicios
+
+#1.1- Calcula el tamaño de muestra para una poblacion de 
+#83 alumnas y alumnos que se encuentran en primer grado
+#de la Licenciatura en Educación Primaria de la BENV.
+
+#1.2.- Una vez que obtengas el tamaño de la muestra, 
+#selecciona el número de lista de las personas que formaran
+#parte de la muestra.
+
+#1.3.- Crea un vector llamado muestra con los números de 
+#lista de las personas que forman parte de la muestra.
+
+#2.- Repite el ejercicio anterios ahora para una poblacion
+# de 90 alumnas y alumnos de integran el segundo año de la
+#licenciatura en Educación Primaria de la BENV.
+
+#3.- Determina el número e identifíca a las personas que participarán
+#en un estudio de seguimiento de egresados de la generación
+#2015-2019 de las 5 licenciaturas de la BENV.
+
+#Primaria: 78 personas egresadas.
+#Preescolar: 84 personas egresadas.
+#Física: 35 personas egresadas.
+#Telesecundaria: 83 personas egresadas.
+#Especial: 25 personas.
+
+
+
+
+
